5afcfb9df6d2de5a6d08944c358d033bebb13dcb
gaurav98094 <kandelgaurav3@gmail.com>
2021-11-20 23:30:02 +0545

    88. Merge Sorted Array

---
+++ LEETCODE/88. Merge Sorted Array/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pointer1 = m-1;
+        int pointer2 = n-1;
+        
+        int curr = nums1.length-1;
+        
+        while(pointer1>=0 && pointer2>=0){
+            
+            if(nums1[pointer1]>nums2[pointer2]){
+                nums1[curr]=nums1[pointer1];
+                pointer1--;
+            }else{
+                nums1[curr]=nums2[pointer2];
+                pointer2--;
+            }
+            curr--;  
+        }
+        
+        
+        while(pointer2>=0){
+            nums1[curr]=nums2[pointer2];
+            pointer2--;
+            curr--;
+        }
+            
+            
+    }
+}
